064d96ad03411922284cb7c09e16ef1dea417870
Westsi <76999267+Westsi@users.noreply.github.com>
2023-06-21 19:55:19 +0100

    started trying to make ast

--- main.go
+++ main.go
@@ -1,30 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
 	// reader := strings.NewReader("edef main() {\n" +
 	// 	"Print(\"Hello World\")\n" +
 	// 	"}")
 
-	reader, err := os.Open("src/main.mn")
-	if err != nil {
-		panic(err)
-	}
-	lexer := NewLexer(reader)
-	var tokens []*LexedTok
-	pos, tok, val := lexer.Lex()
-	tokens = append(tokens, NewLexedTok(pos, tok, val))
-	for tok != EOF {
-		// fmt.Println(pos, tok, val)
-		pos, tok, val = lexer.Lex()
-		tokens = append(tokens, NewLexedTok(pos, tok, val))
-	}
-	// fmt.Println(pos, tok, val)
-	for _, lt := range tokens {
-		fmt.Println(lt.pos, lt.tok, lt.val)
-	}
+	// reader, err := os.Open("src/main.mn")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// lexer := NewLexer(reader)
+	// var tokens []*LexedTok
+	// pos, tok, val := lexer.Lex()
+	// tokens = append(tokens, NewLexedTok(pos, tok, val))
+	// for tok != EOF {
+	// 	// fmt.Println(pos, tok, val)
+	// 	pos, tok, val = lexer.Lex()
+	// 	tokens = append(tokens, NewLexedTok(pos, tok, val))
+	// }
+	// // fmt.Println(pos, tok, val)
+	// for _, lt := range tokens {
+	// 	fmt.Println(lt.pos, lt.tok, lt.val)
+	// }
+	tokens := []*LexedTok{NewLexedTok(Position{1, 1}, IDENT, "main"), NewLexedTok(Position{2, 1}, BLOCKSTART, "{"), NewLexedTok(Position{3, 1}, TYPEANNOT, "string"), NewLexedTok(Position{3, 6}, IDENT, "s"), NewLexedTok(Position{3, 8}, ASSIGN, "="), NewLexedTok(Position{3, 10}, STRINGLITERAL, "Hello World!"), NewLexedTok(Position{4, 1}, BLOCKEND, "}"), NewLexedTok(Position{5, 1}, EOF, "")}
+	_ = Parse(tokens)
 }
