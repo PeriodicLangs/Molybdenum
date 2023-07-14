0988c5bfd6f8555c4ee0f8b920f626b796c14e69
Westsi <76999267+Westsi@users.noreply.github.com>
2023-07-14 20:08:55 +0100

    codegen

--- codegen/gen.go
+++ codegen/gen.go
@@ -1,7 +1,6 @@
 package codegen
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
@@ -14,33 +13,10 @@ var m *ir.Module
 func Gen(program *ast.Program) {
 	m = ir.NewModule()
 	for _, stmt := range program.Statements {
-		generateStatement(stmt)
+		// stmt.Gen()
+		stmt.Literal()
 	}
 	compile()
-}
-
-func generateStatement(stmt ast.Statement) {
-	// might not work
-	switch stmt := stmt.(type) {
-	case *ast.ExpressionStatement:
-		generateExpressionStatement(stmt)
-	case *ast.BlockStatement:
-		generateBlockStatement(stmt)
-	case *ast.ReturnStatement:
-		generateReturnStatement(stmt)
-	}
-}
-
-func generateExpressionStatement(stmt *ast.ExpressionStatement) {
-	fmt.Println("Generating expression statement")
-}
-
-func generateBlockStatement(stmt *ast.BlockStatement) {
-	fmt.Println("Generating block statement")
-}
-
-func generateReturnStatement(stmt *ast.ReturnStatement) {
-	fmt.Println("Generating return statement")
 }
 
 func compile() {
